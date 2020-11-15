58e3a55b0b36154520edeef9018051c84b19c5e4
Nikhil Varma <nikhilvarma892@gmail.com>
2020-11-16 00:37:17 +0530

    Feat: Add color picker

--- src/components/colors/Colors.jsx
+++ src/components/colors/Colors.jsx
@@ -1,8 +1,39 @@
-import React, {PureComponent} from 'react';
+import React, {Component} from 'react';
 import './Colors.scss';
+import {FaSquareFull} from 'react-icons/fa';
+import {Popover} from 'antd';
 
-export default class Colors extends PureComponent {
+export default class Colors extends Component {
+  constructor(props) {
+    super(props);
+    this.colorSet = ['red', 'green', 'blue', 'aqua', 'black'];
+  }
+
+  getColors = (colors) => {
+    return (
+      <div className="colors">
+        {colors.map((c) => (
+          <div className="color" key={c} style={{color: c}} onClick={() => this.setColor(c)}>
+            <FaSquareFull />
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  setColor = (stroke) => {
+    const {updateStrokeColor} = this.props;
+    updateStrokeColor(stroke);
+  };
+
   render() {
-    return <div>AB</div>;
+    const {color} = this.props;
+    return (
+      <Popover content={this.getColors(this.colorSet)} placement="right" trigger="click">
+        <div className="color-set" style={{color}}>
+          <FaSquareFull />
+        </div>
+      </Popover>
+    );
   }
 }
